15c6afca677022930a9f69e2f1bd3549f6282490
Dias-x <andre.d14x@gmail.com>
2025-01-06 12:39:25 +0100

    Add files via upload

---
+++ Person.cs
@@ -0,0 +1,25 @@
+﻿namespace Praxis_Polymorphismus;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public Person(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+    }
+
+    //Methode Polymorhie
+    public void Greet()
+    {
+        Console.WriteLine($"{FirstName} {LastName}: Hallo");
+    }
+
+    //Überladene Methode Polymorphie
+    public void Greet(Person personToGreet)
+    {
+        Console.WriteLine($"{FirstName} {LastName} sagt: Hallo {personToGreet.FirstName} {personToGreet.LastName}");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Praxis_Polymorphismus;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Objekte erstellen
+        Person dias = new Person("Dias", "Helder");
+        Person alves = new Person("Alves", "Andre");
+        
+        //Methode abrufen, mit Überladene Methode
+        dias.Greet(alves);
+        
+        
+
+    }
+}
